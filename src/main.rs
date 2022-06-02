d02c749f6753c50fb9cd39f9f75a4b060ecd215c
rimutaka <max@onebro.me>
2022-06-03 04:51:50 +1200

    Tidied up examples.

--- src/main.rs
+++ src/main.rs
@@ -13,10 +13,9 @@
 ///     * `cargo build --target x86_64-unknown-linux-gnu`
 /// 8. Offline builds: `cargo vendor`
 use serde::Deserialize;
-// use std::fmt::Display;
 
-use anyhow::Error;
-use std::error::Error as StdError;
+// use anyhow::Error;
+// use std::error::Error as StdError;
 // type Error = Box<dyn std::error::Error + Send + Sync + 'static>;
 
 #[derive(Debug, Deserialize)]
@@ -27,6 +26,7 @@ enum Suffixes {
     // MD
 }
 
+#[allow(dead_code)]
 #[derive(Debug, Deserialize)]
 struct IndividualDetails {
     pub given_name: String,
@@ -36,20 +36,19 @@ struct IndividualDetails {
     pub suffix: Suffixes,
 }
 
-#[derive(Debug, Deserialize)]
+// #[derive(Debug, Deserialize)]
+// enum Individual {
+//     Parsed(IndividualDetails),
+//     Simple(&'static str),
+// }
 
-enum Individual {
-    Parsed(IndividualDetails),
-    Simple(&'static str),
-}
-
-// impl Display for Individual {
+// impl std::fmt::Display for Individual {
 //     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
 //         match &self {
 //             Individual::Parsed(v) => {
-//                 write!(f, "Our individual: {} {})", v.given_name, v.family_name)
+//                 write!(f, "Our Parsed individual: {} {}", v.given_name, v.family_name)
 //             }
-//             _ => write!(f, "Can't print this."),
+//             Individual::Simple(v) => write!(f, "Our Simple individual: {}", v),
 //         }
 //     }
 // }
@@ -58,29 +57,28 @@ const INDIVIDUAL_JSON_OR_STRING: &str = r#"{
     "given_name": "Jim",
     "family_name": "Bolger",
     "age": 0,
-    "suffix":"PhD",
+    "suffix":"PhD"
   }"#;
 
-// const individual_json_or_string: &str = "Jim Bolger, PhD";
+// const INDIVIDUAL_JSON_OR_STRING: &str = "Jim Bolger, PhD";
 
 fn main() {
     println!("Hello, world!");
 
-    // let individual = serde_json::from_str::<IndividualDetails>(INDIVIDUAL_JSON_OR_STRING);
+    let individual = serde_json::from_str::<IndividualDetails>(INDIVIDUAL_JSON_OR_STRING);
     // let individual = serde_json::from_str::<IndividualDetails>(INDIVIDUAL_JSON_OR_STRING).unwrap();
     // let individual = serde_json::from_str::<IndividualDetails>(INDIVIDUAL_JSON_OR_STRING).expect("JSON to struct err:");
     // let individual = serde_json::from_str::<IndividualDetails>(INDIVIDUAL_JSON_OR_STRING)?;
+    // let individual = match serde_json::from_str::<IndividualDetails>(INDIVIDUAL_JSON_OR_STRING) {
+    //     Ok(v) => Individual::Parsed(v),
+    //     Err(e) => {
+    //         eprintln!("JSON to struct err: {}", e);
+    //         Individual::Simple(INDIVIDUAL_JSON_OR_STRING)
+    //     }
+    // };
 
-    let individual = match serde_json::from_str::<IndividualDetails>(INDIVIDUAL_JSON_OR_STRING) {
-        Ok(v) => Individual::Parsed(v),
-        Err(e) => {
-            eprintln!("JSON to struct err: {}", e);
-            Individual::Simple(INDIVIDUAL_JSON_OR_STRING)
-        }
-    };
-
-    println!("Our individual: {:?}", individual);
-    // println!("Our individual: {}", individual);
+    println!("IndividualDetails struct: {:?}", individual);
+    // println!("Individual enum: {}", individual);
 
     // if let Individual::Simple(individual_as_str) = individual {
     //     println!("Our individual: {}", individual_as_str);
